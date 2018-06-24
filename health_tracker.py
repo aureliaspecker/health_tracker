06e5907a8ec8a01ced2328a77fb5e1f3ab493e27
aureliaspecker <aureliaspecker@users.noreply.github.com>
2018-06-24 20:11:56 +0100

    Added creating of output file for data points. Added in a while-loop to reiterate initiale menu until user wishes to Exit.

--- health_tracker.py
+++ health_tracker.py
@@ -3,27 +3,32 @@ import sys
 import easyIO as io
 import terminal_commands as terminal
 
-menu_1 = ["New account", "Account1", "Account2"]
+menu_1 = ["New account", "Name1", "Name2"]
 menu_2 = ["Input weight", "View weight", "Exit"]
 message_1 = "Welcome to the Helth Tracker. Please select one of the following options."
-message_2 = "Hello [name]."                                                                 #need to change [name] to account name once accounts have been created
+message_2 = "Hello [Name]."                                                                 #need to change [name] to account name once accounts have been created
 
 def main():
     print message_2
-    user_choice = io.get_user_menu_selection(title = "How can we help you today?", menu = menu_2)
-    if user_choice == 0:
-        weight = weight_input()
-    elif user_choice == 1:
-        print "hello option 2"
-    elif user_choice == 2:
-        print "hello option 3"
-        sys.exit()
+    user_choice = 0
+    while user_choice != 2:
+        user_choice = io.get_user_menu_selection(title = "How can we help you today?", menu = menu_2)
+        if user_choice == 0:
+            weight = weight_input()
+        elif user_choice == 1:
+            print "hello option 2"
+    print "hello option 3"
+    sys.exit()
 
 def weight_input():
-    print "test: calls function weight_input()"
-    user_weight = io.get_user_input(message = "Please input your weight", dtype = float)    #THIS DOESN'T WORK
-    return user_weight
-
+    print "Please input your weight:"
+    user_weight = raw_input()
+    # Create file to write into
+    output_file = open("name1.txt", "a+")
+    # Write to the file
+    output_file.write(user_weight)
+    output_file.write("\n")
+    output_file.close()
 
 # def main():
 #     # user_input=io.get_user_input(message="Enter input: ",dtype="float")
