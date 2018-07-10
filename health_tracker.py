4c982de0b259e74ce66f80079f203fb4684d6690
Aurelia Specker <aspecker@twitter.com>
2018-07-10 11:27:10 +0100

    Test push

--- health_tracker.py
+++ health_tracker.py
@@ -3,24 +3,41 @@ import sys
 import easyIO as io
 import terminal_commands as terminal
 
-menu_1 = ["New account", "Name1", "Name2"]
+menu_1 = ["Sign in", "New user"]
 menu_2 = ["Input weight", "View weight", "Exit"]
 message_1 = "Welcome to the Helth Tracker. Please select one of the following options."
 message_2 = "Hello [Name]."                                                                 #need to change [name] to account name once accounts have been created
 
 def main():
+    menu_one()
+    #menu_two()
+
+def menu_one():
+    while True:
+        user_choice = io.get_user_menu_selection(title = "Please select from the following options", menu = menu_1)
+        if user_choice == 0:
+            print "Credentials"
+            #accounts=terminal.ls(pattern="*.txt")
+            #print accounts                                                                 #list existing accounts OR say there are no existing accounts
+            break
+        elif user_choice == 1:
+            print "blablabla"                                                               #set up an account (asking for name) and creating a .txt file
+            break
+        else: pass
+
+def menu_two():
     print message_2
     user_choice = 0
     while user_choice != 2:
         user_choice = io.get_user_menu_selection(title = "How can we help you today?", menu = menu_2)
         if user_choice == 0:
             weight = weight_input()
-        elif user_choice == 1:
-            print "hello option 2"
-    print "hello option 3"
+        elif user_choice == 1:                                                              #need to input graph visual on long term
+            print "Do this bit later"
+        else: pass
     sys.exit()
 
-def weight_input():
+def weight_input():                                                                         #change it to an array that is fed into python file at start and then append new entries when exiting
     user_weight = io.get_user_input(message = "Please input your weight: ", dtype = float)
     # Create file to write into
     output_file = open("name1.txt", "a+")
@@ -28,17 +45,5 @@ def weight_input():
     output_file.write("{:4.1f}".format(user_weight))
     output_file.write("\n")
     output_file.close()
-
-# def main():
-#     # user_input=io.get_user_input(message="Enter input: ",dtype="float")
-#     # print user_input
-#     #
-#     # menuuuuu=["option a","option b", "option c"]
-#     # user_input=io.get_user_menu_selection(title="Main menu",menu=menuuuuu)
-#     # print user_input
-#     #
-#     # existing_profiles=terminal.ls(pattern=".*.dat")
-#     # print existing_profiles
-
 
 main()
